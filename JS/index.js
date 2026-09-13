eecc86fb0de197cc655bd460f7c2eee9db297845
Lucas <lucasoliveira.prog7@gmail.com>
2026-09-13 14:31:42 -0300

    reajuste no css e ajustado posição do icone

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,34 @@
-const formulario = document.querySelector(".login")
+const formulario = document.querySelector(".login");
 
 const email = document.querySelector("#iemail");
 const senha = document.querySelector("#ipassword");
 
-formulario.addEventListener("submit", function(evento) {
+const mostrar = document.querySelector("#mostrar");
+const imagemOlho = mostrar.querySelector("img");
 
+
+formulario.addEventListener("submit", function(evento) {
     evento.preventDefault();
 
     const usuario = {
         email: email.value,
         senha: senha.value
     };
-
-    console.log(usuario)
+    console.log(usuario);
 });
+
 
 mostrar.addEventListener("click", function() {
 
-    const mostrar = document.querySelector("#mostrar");
-    const imagemOlho = mostrar.querySelector("img");
+    if (senha.type === "password") {
+        senha.type = "text";
 
-    if (ipassword.type === "password") {
-        ipassword.type = "text"
+        imagemOlho.src = "imgs/eye-on.png";
+        imagemOlho.classList.replace("eye-off", "eye-on");
+    } else {
+        senha.type = "password";
 
-        imagemOlho.src = "../imgs/eye-on.png"
-        imagemOlho.classList.replace("eye-off", "eye-on")
-    } else {    
-        ipassword.type = 'password'
-
-        imagemOlho.src = "../imgs/eye-off.svg"
-        imagemOlho.classList.replace("eye-on", "eye-off")
+        imagemOlho.src = "imgs/eye-off.svg";
+        imagemOlho.classList.replace("eye-on", "eye-off");
     }
-})
+});
